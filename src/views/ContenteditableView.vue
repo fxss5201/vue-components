06d4fbc59dfc1e5009d0989aeb4fdfa0268aa4ec
fanyushun <fanyushun@yunwutech.com>
2024-10-09 15:57:52 +0800

    feat: 分割线拖拽方式调整

--- src/views/ContenteditableView.vue
+++ src/views/ContenteditableView.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="content-box">
-    <div class="content-list"></div>
-    <div class="content-driver" @mousedown="driverMousedownFn">
-      <div class="content-driver-top"></div>
-      <div class="content-driver-bottom"></div>
-    </div>
-    <div class="content-editor" ref="editorBoxRef" :style="{ height: `${editorHeight}px` }">
-      <div class="editor-tool">
-        <el-tooltip effect="light" content="截屏" placement="bottom-start">
-          <el-button text @click="doScreenEvent">
-            <el-icon><Crop /></el-icon>
-          </el-button>
-        </el-tooltip>
-      </div>
-      <div class="editor-box" ref="editorRef" contenteditable="true" @keydown.alt="keyupFn"></div>
-      <div class="editor-button">
-        <el-button type="primary">发送</el-button>
-      </div>
-    </div>
+    <Splitpanes horizontal :dbl-click-splitter="false">
+      <Pane size="80" min-size="50">
+        <div class="content-list"></div>
+      </Pane>
+      <Pane size="20" min-size="18">
+        <div class="content-editor">
+          <div class="editor-tool">
+            <el-tooltip effect="light" content="截屏" placement="bottom-start">
+              <el-button text @click="doScreenEvent">
+                <el-icon><Crop /></el-icon>
+              </el-button>
+            </el-tooltip>
+          </div>
+          <div class="editor-box" ref="editorRef" contenteditable="true" @keydown.alt="keyupFn"></div>
+          <div class="editor-button">
+            <el-button type="primary">发送</el-button>
+          </div>
+        </div>
+      </Pane>
+    </Splitpanes>
   </div>
 </template>
 
@@ -25,9 +27,9 @@
 import { ref } from 'vue'
 import { Crop } from '@element-plus/icons-vue'
 import ScreenShot from 'js-web-screen-shot'
-import { useWindowSize } from '@vueuse/core'
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
 
-const editorBoxRef = ref<HTMLDivElement>()
 const editorRef = ref<HTMLInputElement>()
 const editorImgs = ref<string[]>([])
 const keyupFn = (e: KeyboardEvent) => {
@@ -53,89 +55,20 @@ const doScreenEvent = () => {
     }
   })
 }
-
-const { height: windowHeight } = useWindowSize()
-const halfWindowHeight = windowHeight.value / 2
-const editorBoxMinHeight = 150
-const editorHeight = ref(editorBoxMinHeight)
-let mouseDownFlag = ref(false)
-const mouseUpFn: (this: Document, ev: MouseEvent) => void = () => {
-  mouseDownFlag.value = false
-  document.removeEventListener('mousemove', mouseMoveFn)
-  document.removeEventListener('mouseup', mouseUpFn)
-}
-let mouseMoveFn: (this: Document, ev: MouseEvent) => void
-const driverMousedownFn = (e: MouseEvent) => {
-  let startY = e.clientY
-  mouseDownFlag.value = true
-  mouseMoveFn = (ev: MouseEvent) => {
-    if (mouseDownFlag.value) {
-      const currentY = ev.clientY
-      const deltaY = startY - currentY
-      console.log(deltaY)
-      let height = editorHeight.value + deltaY
-      console.log(height)
-
-      if (height < editorBoxMinHeight) {
-        height = editorBoxMinHeight
-      } else if (height > halfWindowHeight) {
-        height = halfWindowHeight
-      }
-
-      editorHeight.value = height
-      ;(editorBoxRef.value as HTMLDivElement).style.height = `${editorHeight.value}px`
-      startY = currentY
-    }
-  }
-
-  document.addEventListener('mousemove', mouseMoveFn)
-  document.addEventListener('mouseup', mouseUpFn)
-}
 </script>
 
 <style lang="scss" scoped>
 .content-box {
   height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
   .content-list {
-    flex: auto;
+    width: 100%;
+    height: 100%;
     overflow-x: hidden;
     overflow-y: auto;
   }
-  .content-driver {
-    height: 5px;
-    background-color: var(--el-menu-border-color);
-    padding: 2px 0;
-    margin: 10px 0;
-    position: relative;
-    cursor: row-resize;
-    &:hover {
-      .content-driver-top,
-      .content-driver-bottom {
-        background-color: #ebe8e8d1;
-      }
-    }
-    .content-driver-top {
-      position: absolute;
-      left: 0;
-      right: 0;
-      top: 0;
-      height: 2px;
-      background-color: #f9f9f9;
-    }
-    .content-driver-bottom {
-      position: absolute;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      height: 2px;
-      background-color: #f9f9f9;
-    }
-  }
   .content-editor {
-    flex: 0 0 auto;
+    width: 100%;
+    height: 100%;
     overflow-x: hidden;
     display: flex;
     flex-direction: column;
@@ -160,6 +93,39 @@ const driverMousedownFn = (e: MouseEvent) => {
 }
 </style>
 <style lang="scss">
+.splitpanes {
+  &__pane {
+    justify-content: center;
+    align-items: center;
+    display: flex;
+  }
+
+  &__splitter {
+    background-color: var(--el-menu-border-color);
+    position: relative;
+  }
+
+  &__splitter:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    transition: 0.4s;
+  }
+  &--vertical > &__splitter:before {
+    left: -4px;
+    right: -4px;
+  }
+  &--horizontal > &__splitter:before {
+    top: -4px;
+    bottom: -4px;
+  }
+  &__splitter:hover:before {
+    background-color: rgba(220, 223, 230, 0.3);
+  }
+}
 .editor-box {
   img {
     max-width: 160px;
