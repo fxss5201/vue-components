5ba63af226cb958b5469558c1b5c209a61ffb41c
fanyushun <fanyushun@yunwutech.com>
2024-10-17 15:49:30 +0800

    feat: 完善 SimpleSwiper

--- src/components/SimpleSwiper.vue
+++ src/components/SimpleSwiper.vue
@@ -1,37 +1,104 @@
 <template>
-  <div class="simple-swiper" ref="simpleSwiper">
-    <div class="simple-swiper-arrow" v-if="isHadButton" @click="doChangeMarginLeft('l')">
-      <el-icon :size="16"><ArrowLeftBold /></el-icon>
-    </div>
-    <div class="simple-swiper-wrapper" ref="simpleSwiperWeapper">
-      <div class="simple-swiper-content" ref="simpleSwiperContent" :style="{ transitionDuration: `${singleChangeDuration}s` }">
-        <slot></slot>
+  <div class="simple-swiper-box">
+    <div class="simple-swiper" ref="simpleSwiper">
+      <div class="simple-swiper-arrow" v-if="isHadButton && prevNextButtonFlag">
+        <el-button :icon="ArrowLeftBold" circle @click="doChangeMarginLeft('l')" :disabled="nowChangeNum === 1" />
+      </div>
+      <div class="simple-swiper-wrapper" ref="simpleSwiperWeapper">
+        <div class="simple-swiper-content" ref="simpleSwiperContent" :style="{ transitionDuration: `${changeDuration}s` }">
+          <slot></slot>
+        </div>
+      </div>
+      <div class="simple-swiper-arrow" v-if="isHadButton && prevNextButtonFlag">
+        <el-button :icon="ArrowRightBold" circle @click="doChangeMarginLeft('r')" :disabled="nowChangeNum === maxCanChangeNum" />
       </div>
     </div>
-    <div class="simple-swiper-arrow" v-if="isHadButton" @click="doChangeMarginLeft('r')">
-      <el-icon :size="16"><ArrowRightBold /></el-icon>
+    <div class="simple-swiper-pagination" v-if="paginationFlag">
+      <el-pagination
+        :current-page="nowChangeNum"
+        :page-count="maxCanChangeNum"
+        layout="prev, pager, next"
+        size="small"
+        background
+        hide-on-single-page
+        @current-change="paginationCurrentChange"
+      ></el-pagination>
+    </div>
+    <div class="simple-swiper-indicators" v-if="props.showIndicators">{{ nowChangeNum }} / {{ maxCanChangeNum }}</div>
+    <div class="simple-swiper-bullets" v-if="bulletsFlag">
+      <template v-for="item in bulletsList" :key="item">
+        <el-tooltip
+          effect="dark"
+          :content="String(item)"
+          placement="bottom"
+          :disabled="!props.bulletsTooltip"
+        >
+          <div class="simple-swiper-bullets-item" :class="{ active: nowChangeNum === item }" @click="doBulletsClick(item)"></div>
+        </el-tooltip>
+      </template>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ArrowLeftBold, ArrowRightBold } from '@element-plus/icons-vue'
-import { nextTick, ref, onMounted, watch } from 'vue'
+import { nextTick, ref, onMounted, watch, computed } from 'vue'
 import { useDebounceFn, useResizeObserver } from '@vueuse/core'
 
 const props = withDefaults(
   defineProps<{
-    itemWidth?: number
+    itemWidth: number
     itemDuration?: number
-    length?: number
+    length: number
+    switchingMethod: 'prevNextButton' | 'pagination' | 'bullets'
+    showIndicators?: boolean
+    bulletsNumber?: number
+    bulletsTooltip?: boolean
   }>(),
   {
-    itemWidth: 100,
     // 单个子项目滚动的耗时，单位ms
     itemDuration: 180,
-    length: 10
+    switchingMethod: 'prevNextButton',
+    showIndicators: false,
+    // 传奇数
+    bulletsNumber: 5,
+    bulletsTooltip: false
   }
 )
+
+const emit = defineEmits<{
+  change: [curNum: number, allNum: number]
+}>()
+
+const prevNextButtonFlag = computed(() => {
+  return props.switchingMethod === 'prevNextButton'
+})
+
+const paginationFlag = computed(() => {
+  return props.switchingMethod === 'pagination'
+})
+
+const bulletsFlag = computed(() => {
+  return props.switchingMethod === 'bullets'
+})
+
+const bulletsList = computed(() => {
+  const listLength = maxCanChangeNum.value < props.bulletsNumber ? maxCanChangeNum.value : props.bulletsNumber
+  const list = Array.from({ length: maxCanChangeNum.value }).map((_, index) => index + 1)
+  if (listLength === props.bulletsNumber) {
+    const halfBulletsNumber = Math.floor(props.bulletsNumber / 2)
+    const nowList: number[] = []
+    if (nowChangeNum.value < halfBulletsNumber + 1) {
+      nowList.push(...list.slice(0, listLength))
+    } else if (nowChangeNum.value > maxCanChangeNum.value - halfBulletsNumber) {
+      nowList.push(...list.slice(maxCanChangeNum.value - listLength))
+    } else {
+      nowList.push(...list.slice(nowChangeNum.value - halfBulletsNumber - 1, nowChangeNum.value + halfBulletsNumber))
+    }
+    return nowList
+  }
+  return list
+})
 
 const simpleSwiper = ref<HTMLDivElement>()
 const simpleSwiperWeapper = ref<HTMLDivElement>()
@@ -39,8 +106,9 @@ const simpleSwiperContent = ref<HTMLDivElement>()
 const isHadButton = ref(false)
 const singleChangeWidth = ref(0)
 const singleChangeDuration = ref(0)
+const changeDuration = ref(0)
 const maxCanChangeNum = ref(0)
-const nowChangeNum = ref(0)
+const nowChangeNum = ref(1)
 
 const debouncedFn = useDebounceFn(() => {
   doCreateSwiper()
@@ -50,7 +118,7 @@ useResizeObserver(simpleSwiper, () => {
 })
 
 watch(
-  () => [props.length, props.itemWidth, props.itemDuration], 
+  () => [props.length, props.itemWidth, props.itemDuration, props.switchingMethod],
   () => {
     doCreateSwiper()
   }
@@ -68,72 +136,119 @@ function doCreateSwiper () {
 function createSwiper () {
   const simpleSwiperWeapperWidth = simpleSwiperWeapper.value?.offsetWidth as number
   const simpleSwiperContentWidth = props.itemWidth * props.length
-  if (simpleSwiperContent.value) {
-    simpleSwiperContent.value.style.width = `${simpleSwiperContentWidth}px`
-    simpleSwiperContent.value.style.marginLeft = '0px'
-    const num = Math.floor(simpleSwiperWeapperWidth / props.itemWidth)
-    singleChangeWidth.value = num * props.itemWidth
-    singleChangeDuration.value = num * props.itemDuration / 1000
-    maxCanChangeNum.value = Math.ceil(props.length / num)
-    nowChangeNum.value = maxCanChangeNum.value
-    let isHadButtonCur = false
-    if (simpleSwiperContentWidth > simpleSwiperWeapperWidth) {
-      isHadButtonCur = true
-    }
-    let isNeedDone = false
-    if (isHadButton.value !== isHadButtonCur) {
-      isNeedDone = true
-    }
-    isHadButton.value = isHadButtonCur
-    if (isNeedDone) doCreateSwiper()
+  simpleSwiperContent.value!.style.width = `${simpleSwiperContentWidth}px`
+  simpleSwiperContent.value!.style.marginLeft = '0px'
+  const num = Math.floor(simpleSwiperWeapperWidth / props.itemWidth)
+  singleChangeWidth.value = num * props.itemWidth
+  singleChangeDuration.value = num * props.itemDuration / 1000
+  maxCanChangeNum.value = Math.ceil(props.length / num)
+  nowChangeNum.value = 1
+  let isHadButtonCur = false
+  if (simpleSwiperContentWidth > simpleSwiperWeapperWidth) {
+    isHadButtonCur = true
   }
+  let isNeedDone = false
+  if (isHadButton.value !== isHadButtonCur) {
+    isNeedDone = true
+  }
+  isHadButton.value = isHadButtonCur
+  if (isNeedDone) doCreateSwiper()
 }
 
 function doChangeMarginLeft (flag: string) {
-  if (!simpleSwiperContent.value) return
-  let nowMarginLeft = simpleSwiperContent.value.style.marginLeft
+  let nowMarginLeft = simpleSwiperContent.value!.style.marginLeft
   let curMarginLeft = nowMarginLeft ? parseInt(nowMarginLeft) : 0
   if (flag === 'r') {
-    if (nowChangeNum.value > 1) {
-      curMarginLeft -= singleChangeWidth.value
-      nowChangeNum.value--
-    }
-  } else {
     if (nowChangeNum.value < maxCanChangeNum.value) {
-      curMarginLeft += singleChangeWidth.value
+      curMarginLeft -= singleChangeWidth.value
       nowChangeNum.value++
     }
+  } else {
+    if (nowChangeNum.value > 1) {
+      curMarginLeft += singleChangeWidth.value
+      nowChangeNum.value--
+    }
   }
-  simpleSwiperContent.value.style.marginLeft = `${curMarginLeft}px`
+  emit('change', nowChangeNum.value, maxCanChangeNum.value)
+  changeDuration.value = singleChangeDuration.value
+  simpleSwiperContent.value!.style.marginLeft = `${curMarginLeft}px`
+}
+
+function paginationCurrentChange (cur: number) {
+  doChangeByPagination(cur)
+}
+
+function doChangeByPagination (cur: number) {
+  if (cur >= 1 && cur <= maxCanChangeNum.value) {
+    changeDuration.value = Math.abs(cur - nowChangeNum.value) * singleChangeDuration.value
+    console.log(changeDuration.value)
+    nowChangeNum.value = cur
+    emit('change', nowChangeNum.value, maxCanChangeNum.value)
+    const curMarginLeft = (cur - 1) * singleChangeWidth.value * -1
+    simpleSwiperContent.value!.style.marginLeft = `${curMarginLeft}px`
+  }
+}
+
+function doBulletsClick (cur: number) {
+  if (nowChangeNum.value !== cur) {
+    doChangeByPagination(cur)
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.simple-swiper {
-  display: flex;
-  align-items: center;
-  .simple-swiper-arrow {
-    flex: 0 0 40px;
-    width: 40px;
-    height: 40px;
-    background: #fff;
-    border-radius: 50%;
-    box-shadow: 0px 0px 6px 0px rgba(75, 75, 75, 0.16);
+.simple-swiper-box {
+  position: relative;
+  .simple-swiper {
     display: flex;
     align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    &:hover {
-      color: var(--el-color-primary);
+    .simple-swiper-arrow {
+      flex: 0 0 40px;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .simple-swiper-wrapper {
+      margin: 0 5px;
+      flex: auto;
+      overflow: hidden;
+      .simple-swiper-content {
+        display: flex;
+        transition: all 1s;
+      }
     }
   }
-  .simple-swiper-wrapper {
-    margin: 0 5px;
-    flex: auto;
-    overflow: hidden;
-    .simple-swiper-content {
-      display: flex;
-      transition: all 1s;
+  .simple-swiper-pagination {
+    display: flex;
+    justify-content: center;
+  }
+  .simple-swiper-indicators {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    z-index: 100;
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 4px 6px;
+    font-size: 12px;
+    border-radius: 4px;
+    color: #fff;
+  }
+  .simple-swiper-bullets {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+   .simple-swiper-bullets-item {
+      flex-shrink: 0;
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background-color: #ccc;
+      margin: 0 4px;
+      cursor: pointer;
+      &:hover, &.active {
+        background-color: var(--el-color-primary);
+      }
     }
   }
 }
--- src/views/SimpleSwiperView.vue
+++ src/views/SimpleSwiperView.vue
@@ -13,12 +13,38 @@
           <el-input-number v-model="form.itemDuration" :min="80" :step="10" />
           <span style="margin-left: 6px;">ms</span>
         </el-form-item>
+        <el-form-item label="切换方式" prop="switchingMethod">
+          <el-select v-model="form.switchingMethod">
+            <el-option value="prevNextButton" label="上一页下一页按钮" />
+            <el-option value="pagination" label="分页器" />
+            <el-option value="bullets" label="圆点" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="进度展示" prop="showIndicators">
+          <el-checkbox v-model="form.showIndicators" />
+        </el-form-item>
+        <el-form-item v-if="form.switchingMethod === 'bullets'" label="圆点个数" prop="bulletsNumber">
+          <el-input-number v-model="form.bulletsNumber" :min="2" />
+          <span style="margin-left: 6px;">圆点很多的时候，仅显示此处设置的个数</span>
+        </el-form-item>
+        <el-form-item v-if="form.switchingMethod === 'bullets'" label="圆点提示" prop="bulletsTooltip">
+          <el-checkbox v-model="form.bulletsTooltip" />
+          <span style="margin-left: 6px;">圆点很多的时候，设置了 圆点个数 ，移上去显示是多少页</span>
+        </el-form-item>
         <el-form-item>
           拖动外框，也可以自适应
         </el-form-item>
       </el-form>
 
-      <SimpleSwiper class="simple-swiper-box" :length="form.length" :item-width="form.itemWidth" :item-duration="form.itemDuration">
+      <SimpleSwiper class="simple-swiper-box"
+        :length="form.length"
+        :item-width="form.itemWidth"
+        :item-duration="form.itemDuration"
+        :switching-method="form.switchingMethod"
+        :show-indicators="form.showIndicators"
+        :bullets-number="form.bulletsNumber"
+        :bullets-tooltip="form.bulletsTooltip"
+        @change="simpleSwiperChange">
         <div class="simple-swiper-item" v-for="item in list" :key="item" :style="{ flex: `0 0 ${form.itemWidth}px`, width: `${form.itemWidth}px`, height: `${form.itemWidth}px` }">
           <div class="item-title">{{ item }}</div>
         </div>
@@ -32,14 +58,22 @@ import ResizeCard from '@/components/ResizeCard.vue'
 import SimpleSwiper from '@/components/SimpleSwiper.vue'
 import { ref } from 'vue';
 
+type switchingMethodType = 'prevNextButton' | 'pagination' | 'bullets'
 const list = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 const form = ref({
   length: list.value.length,
   itemWidth: 100,
-  itemDuration: 180
+  itemDuration: 180,
+  switchingMethod: 'prevNextButton' as switchingMethodType,
+  showIndicators: false,
+  bulletsNumber: 5,
+  bulletsTooltip: false
 })
 function handleChange () {
   list.value = Array.from({ length: form.value.length }, (_, index) => index + 1)
+}
+function simpleSwiperChange (cur: number, all: number) {
+  console.log(cur, all)
 }
 </script>
 
