6a8dd4dd28be3c4f146d2d032b258fd4b288a9ab
fanyushun <fanyushun@yunwutech.com>
2025-02-20 18:49:50 +0800

    feat: 还原 base

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vue-components/',
   plugins: [
     vue(),
     vueJsx(),
