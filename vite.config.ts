fd6c87e391918da5fdb093897b3ac5b96aa818aa
fxss5201 <723107234@qq.com>
2025-03-03 18:25:57 +0800

    feat: 修改vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -51,8 +51,8 @@ export default defineConfig({
     open: true,
     headers: {
       // terminal 终端本地开发时配置
-      'Cross-Origin-Embedder-Policy': 'credentialless',
-      'Cross-Origin-Opener-Policy': 'same-origin',
+      // 'Cross-Origin-Embedder-Policy': 'credentialless',
+      // 'Cross-Origin-Opener-Policy': 'same-origin',
     }
   }
 })
